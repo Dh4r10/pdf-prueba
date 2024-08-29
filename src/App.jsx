b7c9aa56581a3039ce4783510330cad29da15ad6
Dh4rio <marzchelo1@gmail.com>
2024-08-28 22:17:37 -0500

    data de prueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
 import ministerioLogo from '/public/img/logo_minis.png'
 
+const valuesPrint = {
+  tipoGestion: 'Privada',
+  codigoModular: '531133',
+  cursos: [
+    {
+      id: 1,
+      nombre: 'Diseño de Software',
+      unidad_didactica_1: 'Interes',
+      unidad_didactica_2: 'Creatividad',
+      alumnos: [
+        { id: 1, dni: '12345678', nombres: 'Juan Perez', sexo: 'M', edad: 20 },
+        { id: 2, dni: '87654321', nombres: 'Maria Lopez', sexo: 'F', edad: 22 }
+      ]
+    },
+    {
+      id: 2,
+      nombre: 'Gestion de Proyectos',
+      unidad_didactica_1: 'Valores',
+      unidad_didactica_2: 'Imaginacion',
+      alumnos: [
+        {
+          id: 4,
+          dni: '33345678',
+          nombres: 'Diego Francisco',
+          sexo: 'M',
+          edad: 25
+        },
+        { id: 7, dni: '91654321', nombres: 'Sofia Lopez', sexo: 'F', edad: 21 }
+      ]
+    }
+  ]
+}
+
 function App() {
   return (
     <div className='mx-36 border border-black'>
@@ -44,7 +77,9 @@ function App() {
               <div className='w-[50%] bg-[#FFFF00] p-2 border-r border-black'>
                 Tipo de gestion:
               </div>
-              <div className='w-[50%] border p-2'></div>
+              <div className='w-[50%] border p-2'>
+                {valuesPrint.tipoGestion}
+              </div>
             </div>
             <div className='flex w-[50%]'>
               <div className='w-[30%] bg-[#FFFF00] p-2 border-r border-l border-black'>
@@ -218,136 +253,156 @@ function App() {
         </div>
       </section>
 
-      <section>
-        <div className='flex  text-sm h-56'>
-          <div className='flex w-[2%] p-2 justify-center items-center text-center border-r border-b border-black bg-gray-300'>
-            Nº
-          </div>
-          <div className='flex w-[5.5%] p-2 justify-center items-center text-center border-r border-b border-black bg-gray-300'>
-            Número de documento de identidad
-          </div>
-          <div className='flex w-[40.5%] p-2 justify-center items-center text-center border-r border-b border-black bg-gray-300'>
-            APELLIDOS Y NOMBRES (en orden alfabético)
-          </div>
-          <div className='flex w-[3%] p-2 justify-center items-center text-center border-r border-b border-black bg-gray-300'>
-            SEXO (F/M)
-          </div>
-          <div className='flex w-[3%] p-2 justify-center items-center text-center border-r border-b border-black bg-gray-300'>
-            EDAD
-          </div>
-          <div className='text-xs flex flex-col w-[6.05%] border-r border-b border-black'>
-            <div className='flex h-[84.6%] justify-center items-center w-[100%] text-center border-b border-black bg-gray-300'>
-              PERSONA CON DISCAPACIDAD
+      {valuesPrint.cursos.map((curso) => {
+        return (
+          <section key={curso.id}>
+            <div className='flex  text-sm h-56'>
+              <div className='flex w-[2%] p-2 justify-center items-center text-center border-r border-b border-black bg-gray-300'>
+                Nº
+              </div>
+              <div className='flex w-[5.5%] p-2 justify-center items-center text-center border-r border-b border-black bg-gray-300'>
+                Número de documento de identidad
+              </div>
+              <div className='flex w-[40.5%] p-2 justify-center items-center text-center border-r border-b border-black bg-gray-300'>
+                APELLIDOS Y NOMBRES (en orden alfabético)
+              </div>
+              <div className='flex w-[3%] p-2 justify-center items-center text-center border-r border-b border-black bg-gray-300'>
+                SEXO (F/M)
+              </div>
+              <div className='flex w-[3%] p-2 justify-center items-center text-center border-r border-b border-black bg-gray-300'>
+                EDAD
+              </div>
+              <div className='text-xs flex flex-col w-[6.05%] border-r border-b border-black'>
+                <div className='flex h-[84.6%] justify-center items-center w-[100%] text-center border-b border-black bg-gray-300'>
+                  PERSONA CON DISCAPACIDAD
+                </div>
+                <div className='flex h-[15.4%] w-[100%] bg-gray-300'>
+                  <div className='flex w-[50%] p-2 justify-center items-center text-center border-r border-black'>
+                    Si
+                  </div>
+                  <div className='flex w-[50%] p-2 justify-center items-center text-center '>
+                    No
+                  </div>
+                </div>
+              </div>
+              <div className='flex w-[39.95%] border-black  border-b'>
+                <div className='flex w-[85%] border-r border-black '>
+                  <div className='flex flex-col w-[9.09%] border-r border-black'>
+                    <div className='flex h-[84.5%] justify-center items-center transform -rotate-90 origin-center whitespace-nowrap'>
+                      {curso.unidad_didactica_1}
+                    </div>
+                    <div className='flex h-[15.5%] border-t border-black justify-center items-center bg-gray-300'>
+                      2
+                    </div>
+                  </div>
+                  <div className='flex flex-col w-[9.09%] border-r border-black'>
+                    <div className='flex h-[84.5%] justify-center items-center transform -rotate-90 origin-center whitespace-nowrap'>
+                      {curso.unidad_didactica_2}
+                    </div>
+                    <div className='flex h-[15.5%] border-t border-black justify-center items-center bg-gray-300'>
+                      2
+                    </div>
+                  </div>
+                  <div className='flex flex-col w-[9.09%] border-r border-black'>
+                    <div className='flex h-[84.5%] justify-center items-center transform -rotate-90 origin-center whitespace-nowrap'></div>
+                    <div className='flex h-[15.5%] border-t border-black justify-center items-center bg-gray-300'>
+                      3
+                    </div>
+                  </div>
+                  <div className='flex flex-col w-[9.09%] border-r border-black'>
+                    <div className='flex h-[84.5%] justify-center items-center transform -rotate-90 origin-center whitespace-nowrap'></div>
+                    <div className='flex h-[15.5%] border-t border-black justify-center items-center bg-gray-300'>
+                      4
+                    </div>
+                  </div>
+                  <div className='flex flex-col w-[9.09%] border-r border-black'>
+                    <div className='flex h-[84.5%] justify-center items-center transform -rotate-90 origin-center whitespace-nowrap'></div>
+                    <div className='flex h-[15.5%] border-t border-black justify-center items-center bg-gray-300'>
+                      5
+                    </div>
+                  </div>
+                  <div className='flex flex-col w-[9.09%] border-r border-black'>
+                    <div className='flex h-[84.5%] justify-center items-center transform -rotate-90 origin-center whitespace-nowrap'></div>
+                    <div className='flex h-[15.5%] border-t border-black justify-center items-center bg-gray-300'>
+                      6
+                    </div>
+                  </div>
+                  <div className='flex flex-col w-[9.09%] border-r border-black'>
+                    <div className='flex h-[84.5%] justify-center items-center transform -rotate-90 origin-center whitespace-nowrap'></div>
+                    <div className='flex h-[15.5%] border-t border-black justify-center items-center bg-gray-300'>
+                      7
+                    </div>
+                  </div>
+                  <div className='flex flex-col w-[9.09%] border-r border-black'>
+                    <div className='flex h-[84.5%] justify-center items-center transform -rotate-90 origin-center whitespace-nowrap'></div>
+                    <div className='flex h-[15.5%] border-t border-black justify-center items-center bg-gray-300'>
+                      8
+                    </div>
+                  </div>
+                  <div className='flex flex-col w-[9.09%] border-r border-black'>
+                    <div className='flex h-[84.5%] justify-center items-center transform -rotate-90 origin-center whitespace-nowrap'></div>
+                    <div className='flex h-[15.5%] border-t border-black justify-center items-center bg-gray-300'>
+                      9
+                    </div>
+                  </div>
+                  <div className='flex flex-col w-[9.09%] border-r border-black'>
+                    <div className='flex h-[84.5%] justify-center items-center transform -rotate-90 origin-center whitespace-nowrap'></div>
+                    <div className='flex h-[15.5%] border-t border-black justify-center items-center bg-gray-300'>
+                      10
+                    </div>
+                  </div>
+                  <div className='flex flex-col w-[9.09%] '>
+                    <div className='flex h-[84.5%] justify-center items-center transform -rotate-90 origin-center whitespace-nowrap'></div>
+                    <div className='flex h-[15.5%] border-t border-black justify-center items-center bg-gray-300'>
+                      11
+                    </div>
+                  </div>
+                </div>
+                <div className='flex w-[15%] justify-center items-center bg-gray-300'>
+                  <p className='font-semibold text-xs'>Observaciones</p>
+                </div>
+              </div>
             </div>
-            <div className='flex h-[15.4%] w-[100%] bg-gray-300'>
-              <div className='flex w-[50%] p-2 justify-center items-center text-center border-r border-black'>
-                Si
-              </div>
-              <div className='flex w-[50%] p-2 justify-center items-center text-center '>
-                No
-              </div>
-            </div>
-          </div>
-          <div className='flex w-[39.95%] border-black  border-b'>
-            <div className='flex w-[85%] border-r border-black '>
-              <div className='flex flex-col w-[9.09%] border-r border-black'>
-                <div className='flex h-[84.5%] justify-center items-center'></div>
-                <div className='flex h-[15.5%] border-t border-black justify-center items-center bg-gray-300'>
-                  1
-                </div>
-              </div>
-              <div className='flex flex-col w-[9.09%] border-r border-black'>
-                <div className='flex h-[84.5%] justify-center items-center'></div>
-                <div className='flex h-[15.5%] border-t border-black justify-center items-center bg-gray-300'>
-                  2
-                </div>
-              </div>
-              <div className='flex flex-col w-[9.09%] border-r border-black'>
-                <div className='flex h-[84.5%] justify-center items-center'></div>
-                <div className='flex h-[15.5%] border-t border-black justify-center items-center bg-gray-300'>
-                  3
-                </div>
-              </div>
-              <div className='flex flex-col w-[9.09%] border-r border-black'>
-                <div className='flex h-[84.5%] justify-center items-center'></div>
-                <div className='flex h-[15.5%] border-t border-black justify-center items-center bg-gray-300'>
-                  4
-                </div>
-              </div>
-              <div className='flex flex-col w-[9.09%] border-r border-black'>
-                <div className='flex h-[84.5%] justify-center items-center'></div>
-                <div className='flex h-[15.5%] border-t border-black justify-center items-center bg-gray-300'>
-                  5
-                </div>
-              </div>
-              <div className='flex flex-col w-[9.09%] border-r border-black'>
-                <div className='flex h-[84.5%] justify-center items-center'></div>
-                <div className='flex h-[15.5%] border-t border-black justify-center items-center bg-gray-300'>
-                  6
-                </div>
-              </div>
-              <div className='flex flex-col w-[9.09%] border-r border-black'>
-                <div className='flex h-[84.5%] justify-center items-center'></div>
-                <div className='flex h-[15.5%] border-t border-black justify-center items-center bg-gray-300'>
-                  7
-                </div>
-              </div>
-              <div className='flex flex-col w-[9.09%] border-r border-black'>
-                <div className='flex h-[84.5%] justify-center items-center'></div>
-                <div className='flex h-[15.5%] border-t border-black justify-center items-center bg-gray-300'>
-                  8
-                </div>
-              </div>
-              <div className='flex flex-col w-[9.09%] border-r border-black'>
-                <div className='flex h-[84.5%] justify-center items-center'></div>
-                <div className='flex h-[15.5%] border-t border-black justify-center items-center bg-gray-300'>
-                  9
-                </div>
-              </div>
-              <div className='flex flex-col w-[9.09%] border-r border-black'>
-                <div className='flex h-[84.5%] justify-center items-center'></div>
-                <div className='flex h-[15.5%] border-t border-black justify-center items-center bg-gray-300'>
-                  10
-                </div>
-              </div>
-              <div className='flex flex-col w-[9.09%] '>
-                <div className='flex h-[84.5%] justify-center items-center'></div>
-                <div className='flex h-[15.5%] border-t border-black justify-center items-center bg-gray-300'>
-                  11
-                </div>
-              </div>
-            </div>
-            <div className='flex w-[15%] justify-center items-center bg-gray-300'>
-              <p className='font-semibold text-xs'>Observaciones</p>
-            </div>
-          </div>
-        </div>
 
-        {/* Filas de datos */}
-        <div className='flex '>
-          <div className='flex h-8 w-[2%] text-center border-r border-black '></div>
-          <div className='flex h-8 w-[5.5%] text-center border-r border-black'></div>
-          <div className='flex h-8 w-[40.5%] text-center border-r border-black'></div>
-          <div className='flex h-8 w-[3%] text-center border-r border-black'></div>
-          <div className='flex h-8 w-[3%] text-center border-r border-black'></div>
-          <div className='flex h-8 w-[2.95%] text-center border-r border-black'></div>
-          <div className='flex h-8 w-[3.1%] text-center border-r border-black'></div>
-          <div className='flex h-8 w-[3.06%] text-center border-r border-black'></div>
-          <div className='flex h-8 w-[3.1%] text-center border-r border-black'></div>
-          <div className='flex h-8 w-[3.06%] text-center border-r border-black'></div>
-          <div className='flex h-8 w-[3.1%] text-center border-r border-black'></div>
-          <div className='flex h-8 w-[3.1%] text-center border-r border-black'></div>
-          <div className='flex h-8 w-[3.06%] text-center border-r border-black'></div>
-          <div className='flex h-8 w-[3.1%] text-center border-r border-black'></div>
-          <div className='flex h-8 w-[3.06%] text-center border-r border-black'></div>
-          <div className='flex h-8 w-[3.1%] text-center border-r border-black'></div>
-          <div className='flex h-8 w-[3.06%] text-center border-r border-black'></div>
-          <div className='flex h-8 w-[3.2%] text-center border-r border-black'></div>
-          <div className='flex h-8 w-[5.91%] text-center '></div>
-        </div>
+            {/* Filas de datos */}
+            {curso.alumnos.map((alumno, item) => {
+              return (
+                <div className='flex border-b border-black' key={alumno.id}>
+                  <div className='flex h-8 w-[2%] justify-center border-r border-black '>
+                    {item}
+                  </div>
+                  <div className='flex h-8 w-[5.5%] justify-center  border-r border-black'>
+                    {alumno.dni}
+                  </div>
+                  <div className='flex h-8 w-[40.5%] justify-start px-2 border-r border-black'>
+                    {alumno.nombres}
+                  </div>
+                  <div className='flex h-8 w-[3%] justify-center border-r border-black'>
+                    {alumno.sexo}
+                  </div>
+                  <div className='flex h-8 w-[3%] justify-center border-r border-black'></div>
+                  <div className='flex h-8 w-[2.95%] justify-center border-r border-black'></div>
+                  <div className='flex h-8 w-[3.1%] justify-center border-r border-black'></div>
+                  <div className='flex h-8 w-[3.06%] justify-center border-r border-black'></div>
+                  <div className='flex h-8 w-[3.1%] justify-center border-r border-black'></div>
+                  <div className='flex h-8 w-[3.06%] justify-center border-r border-black'></div>
+                  <div className='flex h-8 w-[3.1%] justify-center border-r border-black'></div>
+                  <div className='flex h-8 w-[3.1%] justify-center border-r border-black'></div>
+                  <div className='flex h-8 w-[3.06%] justify-center border-r border-black'></div>
+                  <div className='flex h-8 w-[3.1%] justify-center border-r border-black'></div>
+                  <div className='flex h-8 w-[3.06%] justify-center border-r border-black'></div>
+                  <div className='flex h-8 w-[3.1%] justify-center border-r border-black'></div>
+                  <div className='flex h-8 w-[3.06%] justify-center border-r border-black'></div>
+                  <div className='flex h-8 w-[3.2%] justify-center border-r border-black'></div>
+                  <div className='flex h-8 w-[5.91%] justify-center '></div>
+                </div>
+              )
+            })}
 
-        {/* Más filas según sea necesario */}
-      </section>
+            {/* Más filas según sea necesario */}
+          </section>
+        )
+      })}
     </div>
   )
 }
